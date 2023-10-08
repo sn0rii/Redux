a201893ee2ad3e0f2be7b47996bd31b7324480af
Wojtek Mitan <wojtekmitan91@gmail.com>
2023-10-08 17:11:09 +0200

    postForm

--- redux02/src/App.js
+++ redux02/src/App.js
@@ -1,9 +1,9 @@
-import Counter from "./features/counter/Counter";
+import PostList from "./features/posts/PostList";
 
 function App() {
   return (
     <main className="App">
-      <Counter />
+      <PostList />
     </main>
   );
 }
